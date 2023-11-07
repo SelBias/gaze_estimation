35a27d40a244733c803808be789d66edc78a9e2d
SelBias <jh19984@snu.ac.kr>
2023-11-07 17:52:31 +0900

    typos

--- menet/menets.py
+++ menet/menets.py
@@ -47,6 +47,7 @@ def MeNets(
         make_reproducibility(SEED)
 
     train_gazes_cuda = train_gazes.to(device)
+    test_gazes_cuda = test_gazes.to(device)
     
     train_N = len(train_gazes)
     train_ids_unique = np.unique(train_ids)
@@ -210,9 +211,9 @@ def MeNets(
 
             test_adjusted = test_Gamma @ w_beta
 
-            test_mae_fixed = mae(train_gazes_cuda, test_fixed, is_3d=False, deg=False).item()
-            test_mae  = mae(train_gazes_cuda, test_fixed + test_random, is_3d=False, deg=False).item()
-            test_mae_adjusted  = mae(train_gazes_cuda, test_fixed + test_adjusted, is_3d=False, deg=False).item()
+            test_mae_fixed = mae(test_fixed, test_gazes_cuda, is_3d=False, deg=False).item()
+            test_mae  = mae(test_fixed + test_random, test_gazes_cuda, is_3d=False, deg=False).item()
+            test_mae_adjusted  = mae(test_fixed + test_adjusted, test_gazes_cuda, is_3d=False, deg=False).item()
 
             print(f'{iter}-iter train MAE, NJLL : {train_loss_list[1,best_index]:.4f} deg, {train_loss_list[0,best_index]:.4f}')
             print(f'{iter}-iter test MAE (fixed) : {test_loss_list[0,best_index]:.4f} deg')
