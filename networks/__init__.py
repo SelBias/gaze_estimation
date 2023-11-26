cefacddaaee67beb6aac95cc042849c9db9a2328
SelBias <jh19984@snu.ac.kr>
2023-11-27 01:06:29 +0900

    Remove unused network architectures

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,1 +1,1 @@
-from . import LeNet, ResNet, ResNet_rev, ResNet_batchnorm, VGG16, VGG16_batchnorm, VGG16_simple, VGG16_rev, VGG16_rev_batchnorm, VGG16_rev_simple
+from . import ResNet, ResNet_batchnorm, VGG16_rev
